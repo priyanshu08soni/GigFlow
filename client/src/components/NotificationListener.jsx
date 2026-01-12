512e8a9cc30758ad3d6aaa6c0be4d1224b1d7d23
PRIYANSHU SONI <122470603+priyanshu08soni@users.noreply.github.com>
2026-01-12 17:01:18 +0530

    ddd

--- client/src/components/NotificationListener.jsx
+++ client/src/components/NotificationListener.jsx
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { io } from 'socket.io-client';
-
+import baseURL from '../utils/axiosConfig';
 const NotificationListener = () => {
     const { userInfo } = useSelector((state) => state.auth);
 
     useEffect(() => {
         if (userInfo) {
-            const socket = io('http://localhost:5000');
+            const socket = io(baseURL);
 
             socket.emit('join_user', userInfo._id);
 
